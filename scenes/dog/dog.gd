2f5bcf3fb82405a7ad5935b743b1f60b78898ee0
Martynas Gedutis <martynasgedutis@gmail.com>
2025-02-16 20:53:54 +0200

    dog scene
    set image and add trait text when mouse entered and close whne closed

---
+++ scenes/dog/dog.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+@export var dog_traits: Array[String] = []
+
+
+func _ready() -> void:
+	var sprite = get_node("Area2D/DogSprite")
+	set_random_dog_texture(sprite)
+	$TraitTextBox.hide()
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func set_random_dog_texture(sprite: Sprite2D):
+	var texture_atlas = preload("res://scenes/dog/dog_atlas_texture.tres")
+	var random_number_generator = RandomNumberGenerator.new()
+	var random_collumn = random_number_generator.randi_range(0, 15)
+	var random_row = random_number_generator.randi_range(0, 2)
+	texture_atlas.region = Rect2(random_collumn * 16,random_row * 16,16,16)
+	sprite.texture = texture_atlas
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("entered")
+	var line_count = dog_traits.size()
+	var line_height = 25
+	$TraitTextBox.custom_minimum_size.y = line_count
+	$TraitTextBox.size.y = line_count * line_height
+	$TraitTextBox.text = "\n".join(dog_traits)
+	$TraitTextBox.show()
+
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	print("leaved")
+	$TraitTextBox.hide()
+	pass # Replace with function body.
