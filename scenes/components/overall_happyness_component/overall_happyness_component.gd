1cd66a1879d64034c1be7be7c8053ee733ba2af8
Martynas Gedutis <martynasgedutis@gmail.com>
2025-02-22 12:51:53 +0200

    colorful happiness bar

--- scenes/components/overall_happyness_component/overall_happyness_component.gd
+++ scenes/components/overall_happyness_component/overall_happyness_component.gd
@@ -9,13 +9,42 @@ func _ready() -> void:
 	GameManager.happiness_changed.connect(_on_update_happiness_bar)
 
 
-
 func _process(delta: float) -> void:
 	pass
 
 
 func update_bar_color(value: int):
-	pass
+	var target_fill_color: Color
+	var target_background_color: Color
+	var stylebox_fill = happy_bar.get_theme_stylebox("fill")
+	var stylebox_background = happy_bar.get_theme_stylebox("background")
+
+	if value > 30:
+		target_fill_color = Color(0,1,0)
+		target_background_color = Color(0.683,0.894,0.663)
+	if value < -30:
+		target_fill_color = Color(1,0,0)
+		target_background_color = Color(0.931,0.422,0.332)
+	if value < 30 and value > - 30:
+		target_fill_color = Color(1,1,0.4)
+		target_background_color = Color(0.603,0.619,0.422)
+	if value == -100:
+		target_background_color = Color(1,0,0)
+
+	var tween = create_tween()
+	tween.tween_method(
+		func(color): 
+		stylebox_fill.bg_color = color,
+		stylebox_fill.bg_color,
+		target_fill_color,
+		0.5
+	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.tween_method(
+		func(color): 
+		stylebox_background.bg_color = color, 
+		stylebox_background.bg_color,
+		target_background_color, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+
 
 func show_image(value: int):
 	if happy_bar.value > 0:
@@ -33,9 +62,9 @@ func show_image(value: int):
 
 func _on_update_happiness_bar():
 	var target_value = GameManager.get_happy_metric_value()
-	#var tween = create_tween()
 	
-	#tween.tween_property(happy_bar, "value", target_value, 1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-	happy_bar.value = GameManager.get_happy_metric_value()
+	var tween = create_tween()
+	tween.tween_property(happy_bar, "value", target_value, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+
 	show_image(happy_bar.value)
-	print(happy_bar.value)
+	update_bar_color(happy_bar.value)
