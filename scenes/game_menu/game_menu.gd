2b819fe9a3ce1d712eb822ef9ea74d95f5d5528f
Martynas Gedutis <martynasgedutis@gmail.com>
2025-02-22 18:11:41 +0200

    main menu

--- scenes/game_menu/game_menu.gd
+++ scenes/game_menu/game_menu.gd
@@ -1,11 +1,24 @@
 extends Node2D
+
+@export var press_delay: float = 0.1
+
 @onready var play_button = $PlayButton
+@onready var quit_button = $QuitButton
 
-# Called when the node enters the scene tree for the first time.
+var tree = get_tree()
+
 func _ready() -> void:
-	pass # Replace with function body.
+	play_button.pressed.connect(_on_play_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_play_button_pressed() -> void:
+	var tree = get_tree()
+	await tree.create_timer(press_delay).timeout
+	tree.change_scene_to_file("res://scenes/game_scene/game_scene.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	var tree = get_tree()
+	await tree.create_timer(press_delay).timeout
+	tree.quit()
