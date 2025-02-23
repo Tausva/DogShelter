a446e7a8ec60437bbd2978b165bf2bc9ac39b49f
Martynas Gedutis <martynasgedutis@gmail.com>
2025-02-23 02:04:40 +0200

    minor change

--- scenes/pause_menu/pause_menu.gd
+++ scenes/pause_menu/pause_menu.gd
@@ -23,17 +23,11 @@ func populate_text_box():
 	populate_resource_to_text_box(alfa)
 	populate_resource_to_text_box(barking)
 	populate_resource_to_text_box(energetic)
-	populate_resource_to_text_box(friendly)
 	populate_resource_to_text_box(mischevious)
 	populate_resource_to_text_box(nosy)
 	populate_resource_to_text_box(sleppy)
-	#var name = alfa.get_trait_name(alfa.current_trait)
-	#var hate = []
-	#for hate_trait in alfa.hated_traits:
-		#hate.append(alfa.get_trait_name(hate_trait))
-	#var hate_values = ", ".join(hate)
-	#$Description.text = name + " Hates: " + hate_values
-	
+	populate_resource_to_text_box(friendly)
+
 
 func _on_resume_button_pressed() -> void:
 	await get_tree().create_timer(press_delay).timeout
